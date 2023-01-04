aa01b30ac45c220c365d2e40ed8c63121315de97
Sarthak Verma <55709030+sarthakvdev@users.noreply.github.com>
2023-01-05 01:36:47 +0530

    react query client, provider added and used

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
+// creating client
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    // Providing client to the provider
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />;
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,11 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import { useQuery } from 'react-query';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { stringify } from 'querystring';
 
+/**
+ * 
+ * @returns fetched data from the given URL
+ */
 const fetchData = async () => {
   try {
     const result = await axios.get('https://dummyjson.com/products');
@@ -15,22 +17,14 @@ const fetchData = async () => {
 };
 
 const Home: NextPage = () => {
-  const [stats, setStats] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    fetchData().then((res) => {
-      setStats(res.products);
-      setIsLoading(false);
-      console.log('products', res.products);
-    });
-  }, []);
+  const { isLoading, data, error, isError } = useQuery('products', fetchData);
+  const products = data?.products;
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>react query mini</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>React Query Mini</title>
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center px-20 text-center">
@@ -40,7 +34,7 @@ const Home: NextPage = () => {
           ) : (
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
               <p className="text-2xl font-bold my-5">Products</p>
-              {stats.map((product: any) => (
+              {products.map((product: any) => (
                 <div
                   key={product.id}
                   className="max-w-sm shadow-lg border p-4 border-gray-200 rounded-lg text-left">
